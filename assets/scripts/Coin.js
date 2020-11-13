22274050d8e87fcae6316b1eed8f68cc4037891e
narayanbhattarai@live.com <narayanbhattarai@live.com>
2020-11-13 13:03:16 -0500

    added class to the coin object

---
+++ assets/scripts/Coin.js
@@ -0,0 +1,15 @@
+class Coin {
+  state = 0;
+  constructor(state) {
+    this.state = state;
+  }
+  flip() {
+    console.log("coin.flip() has been invoked.");
+  }
+  toString() {
+    console.log("coin.toString() has been invoked.");
+  }
+  toHTML() {
+    console.log("coin.toHTML() has been invoked.");
+  }
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,25 +1,42 @@
+let body = document.querySelector("body");
 const coinWithoutClass = {
+  state: 0,
 
-    state: 0,
+  flip: function () {
+    return (this.state = Math.floor(Math.random() * 2));
+  },
 
-    flip: function () {
-        console.log("coin.flip() has been invoked.")
-    },
+  toString: function () {
+    if (this.state === 0) {
+      return "Heads";
+    } else {
+      return "Tails";
+    }
+  },
 
-    toString: function () {
-        console.log("coin.toString() has been invoked.")
-    },
+  toHTML: function () {
+    let image = document.createElement("img");
 
-    toHTML: function () {
-        let image = document.createElement('img')
-
-        console.log("coin.toHTML() has been invoked.")
-
-        return image
+    if (this.state === 0) {
+      image.src = "./assets/images/heads.jpg";
+      image.alt = "Heads";
+    } else {
+      image.src = "./assets/images/tails.jpg";
+      image.alt = "Tails";
     }
 
-}
+    return image;
+  },
+};
 
-console.group("coinWithoutClass")
-console.log(coinWithoutClass)
-console.groupEnd()
+console.group("coinWithoutClass");
+console.log(coinWithoutClass);
+console.groupEnd();
+
+debugger;
+
+const coin1 = new Coin(coinWithoutClass.flip());
+const coin2 = new Coin(coinWithoutClass.flip());
+const coin3 = new Coin(coinWithoutClass.flip());
+const coin4 = new Coin(coinWithoutClass.flip());
+console.log(coin1.state, coin2.state, coin3.state, coin4.state);
